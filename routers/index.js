18127d1efd85c4e0a282e2b51a007786c233d937
nohsiwon <98876@naver.com>
2023-11-22 19:26:52 +0900

    feat:[getProducts]상품조회 및 세부 조회

--- routers/index.js
+++ routers/index.js
@@ -1,8 +1,10 @@
-const express = require("express");
-const authRouter = require("./auth.router");
+const express = require('express');
+const authRouter = require('./auth.router');
+const productRouter = require('./products.router');
 
 const apiRouter = express.Router();
 
-apiRouter.use("/auth", authRouter);
+apiRouter.use('/auth', authRouter);
+apiRouter.use('/products', productRouter);
 
 module.exports = apiRouter;
