fc7dc78e67aa3b2dc9915d785c95da5626a0e83d
nohsiwon <98876@naver.com>
2023-11-24 12:20:23 +0900

    main

--- routers/index.js
+++ routers/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const authRouter = require('./auth.router');
 const usersRouter = require('./users.router');
 const productRouter = require('./products.router');
-const usersRouter = require('./users.router');
 const votesRouter = require('./votes.router');
 
 const apiRouter = express.Router();
