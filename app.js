43cd2f98bb3067be7c703072e4157527969d49b6
Jo Park <39757235+joeunparkdev@users.noreply.github.com>
2023-11-24 23:19:12 +0900

    storeReviews backend done

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ const axios = require('axios');
 const app = express();
 const { SERVER_PORT } = require("./constants/app.constant");
 app.use(cookieParser());
-
 // CORS 설정
 const corsOptions = {
   origin: 'http://127.0.0.1:5500', // 허용된 오리진
@@ -43,49 +42,6 @@ app.get('/proxy', async (req, res) => {
     res.status(500).json({ error: '내부 서버 오류' });
   }
 });
-
-// 샘플 리뷰 데이터
-const reviews = [
-  {
-    id: 1,
-    store_name: '좋은편의점',
-    content: '좋은 군것질이 많아서 좋아요!',
-    rating: 4.5,
-    user: 'user123',
-  },
-  {
-    id: 2,
-    store_name: '편리마트',
-    content: '빵 종류가 풍부해서 자주 이용합니다.',
-    rating: 5.0,
-    user: 'user456',
-  },
-];
-
-// 리뷰 라우트
-app.route('/store-reviews')
-  .get((req, res) => {
-    res.json({ reviews });
-  })
-  .post((req, res) => {
-    const { store_name, content, rating } = req.body;
-
-    if (!store_name || !content || !rating) {
-      return res.status(400).json({ message: '유효하지 않은 입력값입니다.' });
-    }
-
-    const newReview = {
-      id: reviews.length + 1,
-      store_name,
-      content,
-      rating,
-      user: 'newUser', // 여기서는 사용자 정보를 임의로 생성
-    };
-
-    reviews.push(newReview);
-
-    res.status(201).json({ status: '성공', data: newReview });
-  });
 
 // 서버 시작
 app.listen(SERVER_PORT, () => {
