6af234f17f31ceca3818d89305851b61f720904d
Jo Park <39757235+joeunparkdev@users.noreply.github.com>
2023-11-24 09:45:43 +0900

    app.js updated

--- app.js
+++ app.js
@@ -1,20 +1,10 @@
 const express = require('express');
-<<<<<<< HEAD
-const { SERVER_PORT, GENDER } = require("./constants/app.constant");
-const apiRouter = require("./routers/index")
-const votesRouter = require('./routers/votes.router.js'); 
-const bodyParser = require('body-parser');
-const path = require('path');
-const cors = require('cors');  // CORS 모듈 추가
-
-=======
 const cors = require('cors');
 const apiRouter = require("./routers/index");
 const { startCronJob } = require('./routers/cron.router');
 const bodyParser = require('body-parser');
 const path = require('path');
 const axios = require('axios');
->>>>>>> 0fa56033dc633fb3b7ddbe7f879b42a883efccc5
 const app = express();
 const { SERVER_PORT } = require("./constants/app.constant");
 
