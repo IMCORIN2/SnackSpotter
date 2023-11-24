9d0f618593946187c12337b4cf1ff46c4615d3ad
Jo Park <39757235+joeunparkdev@users.noreply.github.com>
2023-11-24 10:36:18 +0900

    votes updated

--- routers/index.js
+++ routers/index.js
@@ -2,11 +2,12 @@ const express = require('express');
 const authRouter = require('./auth.router');
 const productRouter = require('./products.router');
 const usersRouter = require('./users.router');
+const votesRouter = require('./votes.router');
 
 const apiRouter = express.Router();
 
 apiRouter.use('/auth', authRouter);
 apiRouter.use('/products', productRouter);
 apiRouter.use('/users', usersRouter);
-
+apiRouter.use('/votes', votesRouter);
 module.exports = apiRouter;
